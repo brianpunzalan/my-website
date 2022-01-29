bc320a51fb22fca6ad048912a73a542ed4b01eec
Brian Punzalan <brian.punzalan@fwd.com>
2022-01-29 12:01:37 +0800

    fixed FOUC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,42 @@
 import React, { ReactElement } from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const styledComponentsSheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            styledComponentsSheet.collectStyles(<App {...props} />),
+        })
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <React.Fragment>
+            {initialProps.styles}
+            {styledComponentsSheet.getStyleElement()}
+          </React.Fragment>
+        ),
+      }
+    } finally {
+      styledComponentsSheet.seal()
+    }
+  }
+
   render(): ReactElement {
     return (
       <Html>
